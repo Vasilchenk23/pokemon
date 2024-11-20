aea1ec2e6b230d2c6c7eb4c7acacaeab214a9ac4
Vasilchenk23 <vasilchenko.maksim374@gmail.com>
2024-11-20 15:09:55 +0200

    add Main component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+    return(
+        <>
+        <header>
+            <img src="./img/logo.png" alt="" className="logo" />
+            <p>Главная</p>
+            <p>Результаты</p>
+        </header>
+        </>
+    );
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+
+export const Main = () => {
+  const [pokemons, setPokemons] = useState({
+    pokemon1: null,
+    pokemon2: null,
+}); 
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/api/pokemon/random");
+        const data = await response.json();
+        setPokemons(data);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+  
+    fetchPokemons();
+  }, []);
+  
+
+  if (!pokemons.pokemon1 || !pokemons.pokemon2) {
+    return ; 
+  }
+
+  return (
+    <div>
+      <h1>Какой покемон красивый?</h1>
+      <div className="block">
+        <div className="first-pokemon">
+          <img src={pokemons.pokemon1.image} alt={pokemons.pokemon1.name} />
+          <button>Он лучше</button>
+        </div>
+        <div className="second-pokemon">
+          <img src={pokemons.pokemon2.image} alt={pokemons.pokemon2.name} />
+          <button>Он лучше</button>
+        </div>
+      </div>
+    </div>
+  );
+};
