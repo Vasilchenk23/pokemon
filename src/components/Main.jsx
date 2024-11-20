897049ad8cefe0fd196822db025c2836ffe6e5da
Vasilchenk23 <vasilchenko.maksim374@gmail.com>
2024-11-20 18:58:27 +0200

    fix vote

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -24,7 +24,7 @@ export const Main = () => {
 
   const voteForPokemon = async (pokemonId, pokemonName) => {
     try {
-      await fetch("http://localhost:3001/vote", {
+      await fetch("https://pokemon-back-production-8a73.up.railway.app/api/pokemon/vote", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ pokemonId, pokemonName }),
