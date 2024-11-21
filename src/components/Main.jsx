5c8a823b24a6ea241f855b817cf3b810a0e43f43
Vasilchenk23 <vasilchenko.maksim374@gmail.com>
2024-11-21 20:24:03 +0200

    add pokemon in db

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -24,17 +24,27 @@ export const Main = () => {
 
   const voteForPokemon = async (pokemonId, pokemonName) => {
     try {
-      await fetch("https://pokemon-back-production-8a73.up.railway.app/api/pokemon/vote", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ pokemonId, pokemonName }),
-      });
-
+      const response = await fetch(
+        "https://pokemon-back-production-8a73.up.railway.app/api/pokemon/vote",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ pokemonId, pokemonName }),
+        }
+      );
+  
+      if (!response.ok) {
+        const errorData = await response.json();
+        console.error("Error voting:", errorData.error);
+        throw new Error(errorData.error || "Unknown error");
+      }
+  
       fetchPokemons(); 
     } catch (error) {
-      console.error("Error voite:", error);
+      console.error("Error voting:", error.message);
     }
   };
+  
   
   if (!pokemons.pokemon1 || !pokemons.pokemon2) {
     return <p>Загрузка покемонов...</p>;
